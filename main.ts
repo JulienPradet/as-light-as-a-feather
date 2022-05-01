ce604baa928a7b745552f8b1b4c246730cabfc1f
Julien Pradet <julien.pradet@gmail.com>
2022-05-01 14:12:31 +0200

    Improve variability

--- main.ts
+++ main.ts
@@ -3,13 +3,13 @@ import { angle } from "canvas-jp/angle";
 import { CanvasJpArc, Circle } from "canvas-jp/Circle";
 import { Color } from "canvas-jp/Color";
 import { distance } from "canvas-jp/distance";
-import { inOutSine } from "canvas-jp/ease";
+import { inOutSine, inSine } from "canvas-jp/ease";
 import { CanvasJpPoint, Point } from "canvas-jp/Point";
 import { translateVector } from "canvas-jp/transform";
-import { mapRange } from "canvas-sketch-util/math";
+import { mapRange, clamp } from "canvas-sketch-util/math";
 
-const width = 1200;
-const height = 1200;
+const width = 600;
+const height = 600;
 const frames = 100;
 
 let palette = [
@@ -27,7 +27,7 @@ canvasJp(
 
     // Elements variables
     const numberOfElements = Math.ceil(
-      mapRange(random.value(), 0, 1, 100, 500)
+      mapRange(random.value(), 0, 1, 150, 450)
     );
     const length = mapRange(
       Math.pow(random.value(), 2),
@@ -43,10 +43,19 @@ canvasJp(
       width / 100,
       width / 20
     );
+    const elementDirection =
+      random.value() > 0.3 ? 0 : ((random.value() - 0.5) * Math.PI) / 2;
+    const placement = random.pick([placeElementCircle, placeElementWave]);
+    const directionFunction = random.pick(
+      [
+        directionConcentric,
+        placement === placeElementCircle ? null : directionFlat,
+      ].filter(Boolean)
+    );
 
     // Deformation of the positions. If everything is at 0, it should be a
     // perfect circle
-    const flowFieldZoom = mapRange(random.value(), 0, 1, 600, 1200);
+    const flowFieldZoom = mapRange(random.value(), 0, 1, width / 2, width);
     const circleDeformationStrength = mapRange(random.value(), 0, 1, 0, 2);
     const sinusoidalDeformationFrequence = mapRange(random.value(), 0, 1, 0, 2);
     const sinusoidalDeformationStrength = mapRange(
@@ -60,11 +69,11 @@ canvasJp(
     // Control over the colors
     // Gradient precision defines how smooth the gradient should be.
     const gradientPrecision = mapRange(
-      Math.pow(random.value(), 5),
+      Math.pow(random.value(), 18),
       0,
       1,
-      1,
-      15
+      0.8,
+      12
     );
     const isMulticolor = random.value() > 0.3;
     const colorDirection =
@@ -109,7 +118,13 @@ canvasJp(
       for (let index = 0; index < numberOfCircles; index++) {
         // Progress 0 => first circle, 1 => last circle
         // the easing makes the shapes a bit more organic.
-        const progress = inOutSine(index / (numberOfCircles - 1));
+        const progress =
+          index < numberOfCircles / 4
+            ? inSine(1 - index / (numberOfCircles / 4))
+            : inOutSine(
+                (index - numberOfCircles / 4) / ((numberOfCircles / 4) * 3 - 1)
+              );
+        const colorProgress = inOutSine(index / (numberOfCircles - 1));
 
         // This first flow field is a way to apply some "wave" deformation on
         // each stroke.
@@ -133,7 +148,8 @@ canvasJp(
           length *
           circleDeformationStrength;
 
-        const direction = angle(center, initialPosition) + Math.PI / 2;
+        // Angle tangent to the circle
+        const direction = directionFunction(initialPosition);
 
         // Moving the circle normally to its next position
         initialPosition = translateVector(
@@ -156,7 +172,11 @@ canvasJp(
 
         element.push(
           Circle(positionWithSmallFlowField, initialWidth * (1 - progress), {
-            color: Color.mix(startColor, endColor, colorDirection(progress)),
+            color: Color.mix(
+              startColor,
+              endColor,
+              colorDirection(colorProgress)
+            ),
             opacity: 1,
           })
         );
@@ -165,11 +185,24 @@ canvasJp(
       return element;
     }
 
-    const grid = [];
+    const directionCenter =
+      random.value() < 0.2
+        ? center
+        : Point(
+            ((random.value() - 0.5) * width) / 3 + width / 2,
+            ((random.value() - 0.5) * height) / 3 + height / 2
+          );
+    function directionConcentric(initialPosition: CanvasJpPoint) {
+      return (
+        angle(directionCenter, initialPosition) + Math.PI / 2 - elementDirection
+      );
+    }
 
-    // Draw many elements. Position them on a sphere randomly. Once projected on a
-    // 2D plan, it makes the center less dense than the outside
-    for (let pointIndex = 0; pointIndex < numberOfElements; pointIndex++) {
+    function directionFlat(initialPosition: CanvasJpPoint) {
+      return elementDirection;
+    }
+
+    function placeElementCircle() {
       const sphereRadius = width / 2 - length - margin;
       const [x, y] = random.onSphere(sphereRadius);
       const elementCenter = Point(x + center.x, y + center.y);
@@ -178,6 +211,29 @@ canvasJp(
       // If it's far from the center, make it smaller.
       const distanceFromCenter = distance(center, elementCenter);
       const progress = distanceFromCenter / sphereRadius;
+      return { progress, elementCenter, distanceFromCenter };
+    }
+
+    function placeElementWave() {
+      const elementCenter = Point(
+        random.value() * width,
+        random.value() * height
+      );
+      const distanceFromCenter = distance(center, elementCenter);
+      const progress = 1 - distanceFromCenter / (width / 2);
+      return {
+        progress,
+        elementCenter,
+        distanceFromCenter,
+      };
+    }
+
+    const grid = [];
+
+    // Draw many elements. Position them on a sphere randomly. Once projected on a
+    // 2D plan, it makes the center less dense than the outside
+    for (let pointIndex = 0; pointIndex < numberOfElements; pointIndex++) {
+      const { progress, elementCenter, distanceFromCenter } = placement();
 
       const element = makeElement(
         length * progress * random.gaussian(1, 0.1),
@@ -189,7 +245,10 @@ canvasJp(
     }
 
     // Draw the outer elements first. This helps creating some sort of depth feeling
-    grid.sort((a, b) => a.position - b.position);
+    if (directionFunction !== directionFlat) {
+      console.log("sort");
+      grid.sort((a, b) => a.position - b.position);
+    }
 
     return {
       background: mainColor,
@@ -199,12 +258,12 @@ canvasJp(
   {
     width: width,
     height: height,
-    resolution: 1,
+    resolution: 2,
     title: "Wave",
     animation: false,
     numberOfFrames: frames,
     loop: false,
-    exportSketch: false,
+    exportSketch: true,
     embed: false,
     interactive: false,
   }
